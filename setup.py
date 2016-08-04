9443c571d2554bfe573c75019c59b5df0a167171
ethanluoyc <ethanluoyc@gmail.com>
2016-08-04 23:46:28 +0800

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='statsnba-playbyplay',
-    version='0.1.1',
+    version='0.2.0',
     description="Package for parsing play-by-play data from stats.nba.com",
     long_description=readme,
     author="Yicheng Luo",
--- statsnba/__init__.py
+++ statsnba/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Yicheng Luo'
 __email__ = 'ethanluoyc [AT] gmail DOT com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 
 from .api import Api
